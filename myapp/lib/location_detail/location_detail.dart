b154479dea956f5dfdbe759fe9587de159240bcf
Sheikh Aman <amansk664@gmail.com>
2021-11-07 00:23:33 +0600

    Column, mainAxisAlignment, crossAxisAlignment,container

---
+++ myapp/lib/location_detail/location_detail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  const LocationDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            child: Text('hi'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+            child: Text('hi'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('hi'),
+          ),
+        ],
+      ),
+    );
+  }
+}
