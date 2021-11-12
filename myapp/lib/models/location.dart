792e4928e9d507ae53dd34194ce73e604653b847
Sheikh Aman <amansk664@gmail.com>
2021-11-12 14:07:45 +0600

    fetchAll

--- myapp/lib/models/location.dart
+++ myapp/lib/models/location.dart
@@ -4,19 +4,17 @@ class Location {
   String name;
   String imagePath;
   final List<LocationFact> facts;
-  Location(this.name,this.imagePath,this.facts);
+  Location(this.name, this.imagePath, this.facts);
 
   static List<Location> fetchAll() {
+    //fetchAll function created
     return [
       Location('Arashiyama Bamboo Grove', 'assets/images/kiyomizu-dera.jpg', [
-        
-      
-      LocationFact('Summary', 
-      'While we could go on about the ethereal glow and seeminggly endless heights of this bamboo grove on the outskirts of Kyoto, the sights pleasures extends beyond the  vsual realm', [
-        
-      
-      LocationFact('How to Get There', 'Kuoto airport, with several terminals, is located 16 kilometers of the city and is also know as kyoto. Kyoto can also be reached by transport links from other regional  airports, 
-    
-
-
- 
+        LocationFact('Summary',
+            'While we could go on about the ethereal glow and seemingly endless heights of this bamboo grove on the outskirts of Kyoto, the sight\'s pleasures extend beyond the visual realm.'),
+        LocationFact('How to Get There',
+            'Kyoto airport, with several terminals, is located 16 kilometres south of the city and is also known as Kyoto. Kyoto can also be reached by transport links from other regional airports.'),
+      ]),
+    ];
+  }
+}
